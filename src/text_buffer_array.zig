53aa9454c7e516e1bab014e9eb4c9e97e60d775a
Dmitry Matveyev <dev@greenfork.me>
2021-11-06 22:13:30 +0600

    Next char and prev char

--- src/text_buffer_array.zig
+++ src/text_buffer_array.zig
@@ -7,8 +7,14 @@ const kisa = @import("kisa");
 const assert = std.debug.assert;
 const testing = std.testing;
 const Zigstr = @import("zigstr");
+const unicode = std.unicode;
 
 pub const Contents = Zigstr;
+
+fn isTrailing(ch: u8) bool {
+    // Byte 2,3,4 are all of the form 10xxxxxx. Only the 1st byte is not allowed to start with 0b10.
+    return ch >> 6 == 0b10;
+}
 
 pub const Buffer = struct {
     contents: Contents,
@@ -45,22 +51,68 @@ pub const Buffer = struct {
     pub fn deinit(self: *Self) void {
         self.contents.deinit();
     }
+
+    pub fn nextCharPos(self: Self, offset: usize) usize {
+        // -1 for maximum offset, another -1 so we can add +1 to it.
+        if (offset >= self.contents.bytes.items.len - 2) return offset;
+        var result = offset + 1;
+        while (isTrailing(self.contents.bytes.items[result]) and
+            result < std.math.maxInt(usize) - 1)
+        {
+            result += 1;
+        }
+        return result;
+    }
+
+    pub fn prevCharPos(self: Self, offset: usize) usize {
+        if (offset == 0) return 0;
+        var result = offset - 1;
+        while (isTrailing(self.contents.bytes.items[result]) and result > 0) result -= 1;
+        return result;
+    }
 };
 
-test "state2: init text buffer with file descriptor" {
+test "state: init text buffer with file descriptor" {
     var file = try std.fs.cwd().openFile("kisarc.zzz", .{});
     defer file.close();
-    var text_buffer = try initContentsWithFile(
+    var buffer = try Buffer.initWithFile(
         testing.allocator,
         file,
     );
-    defer text_buffer.deinit(testing.allocator);
+    defer buffer.deinit();
 }
 
-test "state2: init text buffer with text" {
-    var text_buffer = try initContentsWithText(
-        testing.allocator,
-        "Hello",
-    );
-    defer text_buffer.deinit(testing.allocator);
+test "state: init text buffer with text" {
+    var buffer = try Buffer.initWithText(testing.allocator, "Hello");
+    defer buffer.deinit();
+}
+
+test "state: nextCharPos" {
+    var buffer = try Buffer.initWithText(testing.allocator, "Dobrý deň");
+    defer buffer.deinit();
+    try testing.expectEqual(@as(usize, 1), buffer.nextCharPos(0));
+    try testing.expectEqual(@as(usize, 2), buffer.nextCharPos(1));
+    try testing.expectEqual(@as(usize, 3), buffer.nextCharPos(2));
+    try testing.expectEqual(@as(usize, 4), buffer.nextCharPos(3));
+    try testing.expectEqual(@as(usize, 6), buffer.nextCharPos(4)); // ý, 2 bytes
+    try testing.expectEqual(@as(usize, 6), buffer.nextCharPos(5)); // error condition
+    try testing.expectEqual(@as(usize, 7), buffer.nextCharPos(6));
+    try testing.expectEqual(@as(usize, 8), buffer.nextCharPos(7));
+    try testing.expectEqual(@as(usize, 9), buffer.nextCharPos(8));
+    try testing.expectEqual(@as(usize, 9), buffer.nextCharPos(9));
+}
+
+test "state: prevCharPos" {
+    var buffer = try Buffer.initWithText(testing.allocator, "Dobrý deň");
+    defer buffer.deinit();
+    try testing.expectEqual(@as(usize, 8), buffer.prevCharPos(9));
+    try testing.expectEqual(@as(usize, 7), buffer.prevCharPos(8));
+    try testing.expectEqual(@as(usize, 6), buffer.prevCharPos(7));
+    try testing.expectEqual(@as(usize, 4), buffer.prevCharPos(6)); // ý, 2 bytes
+    try testing.expectEqual(@as(usize, 4), buffer.prevCharPos(5)); // error condition
+    try testing.expectEqual(@as(usize, 3), buffer.prevCharPos(4));
+    try testing.expectEqual(@as(usize, 2), buffer.prevCharPos(3));
+    try testing.expectEqual(@as(usize, 1), buffer.prevCharPos(2));
+    try testing.expectEqual(@as(usize, 0), buffer.prevCharPos(1));
+    try testing.expectEqual(@as(usize, 0), buffer.prevCharPos(0));
 }
